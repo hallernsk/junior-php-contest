6ae043bd92e5e2d61f42f64eff1bd46b1871a2d9
hallernsk <hallaer@ngs.ru>
2022-08-20 20:18:41 +0700

    connected to DB(sqlite)

--- src/db/connection.php
+++ src/db/connection.php
@@ -2,14 +2,17 @@
 
 namespace App\db\connection;
 
+use PDO;
+
 function createConnection()
 {
     $dbPath = __DIR__ . '/../../db.sqlite';
     touch($dbPath);
 
-    $db = null;
+    //$db = null;
 
     //TODO: Create connection to Sqlite DB
+    $db = new PDO('sqlite:' . $dbPath );
 
     return $db;
 }
